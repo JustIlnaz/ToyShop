13ea9bec6c27ae503208f04b4129b296dbf8e715
202323 <202323@DESKTOP-4SFIC8R>
2025-06-04 14:31:19 +0300

    начало проекта Магазин игрушек

---
+++ ToyShop/Pages/SignUpPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Controls;
+
+namespace ToyShop.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для SignUpPage.xaml
+    /// </summary>
+    public partial class SignUpPage : Page
+    {
+        public SignUpPage()
+        {
+            InitializeComponent();
+        }
+
+
+    }
+}
