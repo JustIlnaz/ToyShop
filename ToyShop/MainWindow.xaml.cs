e3cbfd242ce0531dfb3edc9e1c6183c49bc05a56
Ильназ <ilnazikwork@gmail.com>
2025-06-06 21:47:00 +0300

    fixed

--- ToyShop/MainWindow.xaml.cs
+++ ToyShop/MainWindow.xaml.cs
@@ -25,7 +25,7 @@
                 public MainWindow()
                 {
                     InitializeComponent();
-                    MainFrame.Navigate(new AuthorizationPage());
+                   
                 }
             }
 }
